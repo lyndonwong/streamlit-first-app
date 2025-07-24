b48f06874c1864da6f17c83c030c153e3150a1dd
Lyndon Wong <lyndonwong@users.noreply.github.com>
2025-07-24 05:23:16 +0000

    WIP on Streamlit basic concepts tutorial. (https://opulent-umbrella-5469vqrrgp3vxgv.github.dev/)

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,41 @@ data = pd.DataFrame({
  
 ## Create a map with the data
 st.map(data, zoom=1)
+
+"""
+### Continue with streamlit get-started guide Basic Concepts
+Create a table with data:    
+"""
+
+df=pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+df
+
+# write table manually as well
+st.write(df)
+
+# writing an interactive table
+import numpy as np
+
+dataframe = np.random.randn(5,7)
+st.dataframe(dataframe)
+
+# interactive table that highlights minimum values in each column
+dataframe = pd.DataFrame(
+    np.random.randn(11, 17),
+    columns=('col %d' % i for i in range(17)),
+    index=('row %d' % i for i in range(11))      
+)   
+
+st.dataframe(dataframe.style.highlight_min(axis=0)) 
+
+# generate static table with st.table function
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe) 
+
