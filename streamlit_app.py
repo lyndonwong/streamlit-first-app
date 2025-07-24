997876eeebbb8de0a384a62c23f381d936545941
Lyndon Wong <lyndonwong@users.noreply.github.com>
2025-07-24 16:45:57 +0000

    Adds tables, charts, maps and text elements.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("🎈 Lyndon's first streamlit app in the cloud!")
 st.write(
@@ -31,12 +32,14 @@ df
 st.write(df)
 
 # writing an interactive table
-import numpy as np
 
 dataframe = np.random.randn(5,7)
 st.dataframe(dataframe)
 
 # interactive table that highlights minimum values in each column
+
+st.subheader("Interactive Table that highlights minimum values in each column") 
+
 dataframe = pd.DataFrame(
     np.random.randn(11, 17),
     columns=('col %d' % i for i in range(17)),
@@ -46,9 +49,38 @@ dataframe = pd.DataFrame(
 st.dataframe(dataframe.style.highlight_min(axis=0)) 
 
 # generate static table with st.table function
+st.subheader("Static Table Example")    
 
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe) 
+
+# Draw a line chart
+
+st.subheader("Line Chart Example")
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['apha', 'beta', 'gamma']
+)   
+
+st.line_chart(chart_data)
+
+# display table for above chart
+st.text("Data table for above chart")
+st.dataframe(chart_data)
+
+# Plot a map
+st.subheader("Map Plot")
+map_data = pd.DataFrame(
+    np.random.randn(80, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+st.map(map_data)    
+
+st.caption("Map data is generated randomly, so it will change every time you refresh the page.")
+st.subheader("Lat Lon data table for above map")
+st.dataframe(map_data)
+
 
